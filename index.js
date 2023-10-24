8d20679d526c34dae54206eeccaa3ed3e43366bc
Xuemei Huang <mei.huang9500@gmail.com>
2023-10-24 15:04:51 +0900

    display products

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ import { getElement } from "./src/utils.js";
 
 const init = async () => {
   const products = await fetchProducts();
-  console.log(products);
+  if (products) {
+    // add products to the store
+    setupStore(products);
+    const featured = store.filter((product) => product.featured === true);
+    display(featured, getElement(".featured-center"));
+  }
 };
 window.addEventListener("DOMContentLoaded", init);
